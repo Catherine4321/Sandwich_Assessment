9ba7263d415cc8e1120dd4bb8d23ee3277fec858
Catherine4321 <catherine.rombel@marsden.school.nz>
2023-06-08 16:27:29 +1200

    Added information about first 4 sprints to planning website

--- Sandwich.py
+++ Sandwich.py
@@ -21,12 +21,18 @@ order_number = 0
 
 def return_to_menu():
     global running_order
-    ask = input("would you like to go back to the menu? y for yes, n for no: ")
-    if ask == 'y':
-        running_order = True
-    elif ask == 'n':
-        running_order = False
-        print("Thank-you for using our system.")
+    get_ask = True
+    while get_ask == True:
+        ask = input("would you like to go back to the menu? y for yes, n for no: ")
+        if ask == 'y':
+            running_order = True
+            get_ask = False
+        elif ask == 'n':
+            print("Thank-you for using our system.")
+            running_order = False
+            get_ask = False
+        else:
+            print("That's not an answer I was expecting. Please try again.")
 
 #function that shows sandwich menu
 def show(L):
@@ -61,16 +67,15 @@ def modify():
                     print("Please type a number.")
             #loop to make sure that the sandwich order is under 5
             while get_samount == True:
-
                 try:
                     user_amount = int(input("How many of this sandwich do you want: "))
                     if user_amount + user_order > 5:
-                        print("You may only order 5 sandwiches.")
+                        print("You may only order 5 sandwiches. You are currently ordering {} sandwiches".format(user_order))
                     elif user_amount + user_order <= 5:
                         sandwiches[user_choice-1][2] = sandwiches[user_choice-1][2] + user_amount
                         user_order += user_amount
-                        get_samount = False
                         print("Thank-you for adding {} {}".format(sandwiches[user_choice-1][2], sandwiches[user_choice - 1][0]))
+                        get_samount = False
                         if user_order == 5:
                             print("As you have 5 sandwiches in your order, you shall move to the paying process.")
                             get_sandwich = False
@@ -90,16 +95,18 @@ def modify():
                 except ValueError:
                     print("Please only enter numbers.")
         #checks to see if order is at 5 sandwiches or not
-        if user_order == 5:
+        elif user_order == 5:
             print("As you have 5 sandwiches in your order, you shall be moved to the paying process.")
             pay()
-    #allows user to remove items from their order
+        #allows user to remove items from their order
         elif order_modify == 'r':
             review()
             remove = int(input("Please enter the number of the sandwich you would like to remove from your order: "))
             remove_amount = int(input("Please enter how many you would like to remove: "))
             sandwiches[remove - 1][2] = sandwiches[remove - 1][2] - remove_amount
             print("Thank-you. There are now {} {} on your order.".format(sandwiches[remove -1][2], sandwiches[remove - 1][0]))
+        else:
+            print("That wasn't an answer I was expecting. Please try again.")
 
 
 #function that allows users to review their order
@@ -134,9 +141,13 @@ def pay():
     pick_or_deliver = input("Would you like to pick up your order, or have it delivered? There is a $3 delivery fee. p for pick up, d for delivery: ")
     if pick_or_deliver == 'p':
         print("Thank-you for your order. Your order is order #{}, and should take about {} minutes.".format(order_number, sandwich_amount * 10))
-        print("We will call you on this number: {}".format(user_phone))
+        print("We will call you on this number when your order is complete: {}".format(user_phone))
     if pick_or_deliver == 'd':
+        address = input("Please input your address here: ")
         cost += 3
+        print("Thank-you {}. Your order is order number {}. The total now comes to ${}".format(user_name, order_number, cost))
+        print("Your order should be delivered to {} in {} minutes.".format(address, (sandwich_amount + 1) * 10))
+        print("If there any issues with your order, we will call this number: {}".format(user_phone))
     print("This order has been fulfilled, any sandwiches ordered now will go to a new order.")
     for i in range(0, len(sandwiches)-1):
         if sandwiches[i][2] > 0:
